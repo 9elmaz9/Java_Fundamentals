db2cda1b0cc4a04059a40e9e5972c41e755e8b92
9elmaz9 <132524901+9elmaz9@users.noreply.github.com>
2023-09-20 09:24:00 +0200

    Add files via upload

---
+++ Java_Fundamentals/src/be/intecbrussel/Les1/ByteExapmle.java
@@ -0,0 +1,13 @@
+package be.intecbrussel.Les1;
+
+public class ByteExapmle {
+
+    public static void main(String[] args) {
+
+        byte a = 10;
+        byte b = -20;
+
+        System.out.println("De waarde van variabel a=" +a);
+        System.out.println("De waarde van variabel b = "+b);
+    }
+}
---
+++ Java_Fundamentals/src/be/intecbrussel/Les2/Oefening2.java
@@ -0,0 +1,13 @@
+package be.intecbrussel.Les2;
+
+public class Oefening2 {
+
+    public static void main(String[] args) {
+
+        String name ="True!";
+
+
+        System.out.println("True!");
+
+    }
+}
