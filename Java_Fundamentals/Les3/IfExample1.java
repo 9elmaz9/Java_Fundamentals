9cde55112577cd21694cf843e3505faea4cf748a
9elmaz9 <132524901+9elmaz9@users.noreply.github.com>
2023-09-20 15:40:16 +0200

    Add files via upload

---
+++ Java_Fundamentals/Les3/IfExample1.java
@@ -0,0 +1,16 @@
+package be.intecbrussel.Les3;
+
+public class IfExample1 {
+    public static void main(String[] args) {
+
+        int number= 110;
+        if(number<100){
+
+            System.out.println("The value of numberis smaller than 100");
+        }
+        else{
+
+            System.out.println("The value of number is NOT smaller than 100");
+        }
+    }
+}
---
+++ Java_Fundamentals/Les3/Oefening1.java
@@ -0,0 +1,17 @@
+package be.intecbrussel.Les3;
+
+public class Oefening1 {
+    public static void main(String[] args) {
+
+
+        float a = 12.5F;
+        float b=7.8F;
+
+        float mul=a*b;
+
+        System.out.println("Multiplication of A and B=" + mul);
+
+
+
+    }
+}
