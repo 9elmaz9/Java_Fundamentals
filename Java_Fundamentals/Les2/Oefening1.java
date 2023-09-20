ca854c03309b6e20903daf5332a90083a581ee6e
9elmaz9 <132524901+9elmaz9@users.noreply.github.com>
2023-09-20 15:39:52 +0200

    Add files via upload

---
+++ Java_Fundamentals/Les2/Oefening1.java
@@ -0,0 +1,12 @@
+package be.intecbrussel.Les2;
+
+public class Oefening1 {
+
+    public static void main(String[] args) {
+
+        int a = 7;
+
+        System.out.println(a);
+    }
+
+}
---
+++ Java_Fundamentals/Les2/Oefening2.java
@@ -0,0 +1,13 @@
+package be.intecbrussel.Les2;
+
+public class Oefening2 {
+
+    public static void main(String[] args) {
+
+        Boolean name = true;
+
+
+        System.out.println("Name:" +name);
+
+    }
+}
