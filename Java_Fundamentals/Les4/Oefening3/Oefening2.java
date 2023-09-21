5afd3495a78e1d56df1f8e1e6379d863fcb8c385
9elmaz9 <132524901+9elmaz9@users.noreply.github.com>
2023-09-21 15:41:01 +0200

    Add files via upload

---
+++ Java_Fundamentals/Les4/Oefening3/Oefening2.java
@@ -0,0 +1,20 @@
+package be.intecbrussel.Les4.Oefening3;
+
+public class Oefening2 {
+    public static void main(String[] args) {
+
+        int input = 10;
+
+        if (input > 0) {
+            System.out.println("input is positive");
+        }
+        if (input < 0) {
+            System.out.println("input is negative");
+
+        } else {
+            System.out.println("input is not equal to 0");
+
+        }
+    }
+}
+
