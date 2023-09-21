5d04bd868041f503f5f582eed76fa859e1d796bb
9elmaz9 <132524901+9elmaz9@users.noreply.github.com>
2023-09-21 15:40:47 +0200

    Add files via upload

---
+++ Java_Fundamentals/Les3/Oefening2/Oefening3.java
@@ -0,0 +1,25 @@
+package be.intecbrussel.Les3.Oefening2;
+
+public class Oefening3 {
+
+    public static void main(String[] args) {
+
+        byte a = 10;
+        int b = 5;
+        long c = 5;
+
+        boolean equal = (b == c);
+        boolean notEqual = (a != b);
+        boolean biggerThan = (a > c);
+        boolean smallerThan = (b < a);
+        boolean biggerEqual = (a >= b);
+        boolean smallerEqual = (c <= a);
+
+        System.out.println("a <b:" + (a < b));
+        System.out.println("a != c:" + (a + b));
+        System.out.println("a> c:" + (a + c));
+        System.out.println("b< a:" + (b + a));
+        System.out.println("a>=b:" + (a + b));
+        System.out.println("c<=a:" + (c + a));
+    }
+}
---
+++ Java_Fundamentals/Les3/Oefening2/Oefening4.java
@@ -0,0 +1,18 @@
+package be.intecbrussel.Les3.Oefening2;
+
+public class Oefening4 {
+
+    public static void main(String[] args) {
+
+        short shortNum = 101;
+        float floatNum = 10.99F;
+        boolean booVar = true;
+
+
+        System.out.println("floatNum += floatNum+shortNum:");
+        System.out.println("floatNum+= floatNum+shortNum:");
+        System.out.println("booVar=true:");
+
+
+    }
+}
