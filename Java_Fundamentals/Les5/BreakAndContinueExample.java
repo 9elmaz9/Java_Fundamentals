bc54effa4beb2d2dd43300ec3ff5a0d8268a1ed8
9elmaz9 <132524901+9elmaz9@users.noreply.github.com>
2023-09-21 15:41:52 +0200

    Add files via upload

---
+++ Java_Fundamentals/Les5/BreakAndContinueExample.java
@@ -0,0 +1,17 @@
+package be.intecbrussel.Les5;
+
+public class BreakAndContinueExample {
+    public static void main(String[] args) {
+
+        for (int index=0;index<10;index++){
+
+            if(index==4) {
+                break;
+
+            }
+            System.out.println(index);
+            }
+        }
+    }
+
+
