c4e2c43cfad27552a10383a7e75453b656de284b
9elmaz9 <132524901+9elmaz9@users.noreply.github.com>
2023-10-03 15:48:36 +0200

    Add files via upload

---
+++ Chapter02_OOP/src/intecbrussel/Les3/Oefening2/Oefening1.java
@@ -0,0 +1,17 @@
+package intecbrussel.Les3.Oefening2;
+
+public class Oefening1 {
+    public static void main(String[] args) {
+
+
+        float a = 12.5F;
+        float b=7.8F;
+
+        float mul=a*b;
+
+        System.out.println("Multiplication of A and B=" + mul);
+
+
+
+    }
+}
---
+++ Chapter02_OOP/src/intecbrussel/Opdracht1/ForExample1.java
@@ -0,0 +1,16 @@
+package intecbrussel.Opdracht1;
+
+public class ForExample1 {
+
+    public static void main(String[] args) {
+
+        /*for (int count = 400; count >= 350; count--) {
+            System.out.println("The counter is:" + count);        1 version*/
+
+        for ( int i = 400; i>=350; --i){
+            System.out.println(i);
+
+        }
+    }
+}
+
