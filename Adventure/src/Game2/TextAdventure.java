683161d8601319478fe2be973f9f4ac5e8b65310
9elmaz9 <132524901+9elmaz9@users.noreply.github.com>
2023-11-02 00:45:32 +0100

    Add files via upload

---
+++ Adventure/src/Game2/TextAdventure.java
@@ -0,0 +1,9 @@
+package Game2;
+//class for launching the game
+public class TextAdventure {
+
+    public static void main(String[] args) {
+        AdventureGame adventureGame=new AdventureGame(); // Create a game instance// Создаем экземпляр игры
+        adventureGame.startAdventure(); //start // Запускаем приключение
+    }//startAdventure()содержит основной цикл игры =contains the main game loop
+}
