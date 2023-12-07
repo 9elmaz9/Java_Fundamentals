f7f572545fc4be432e47568d892a50c73eea96f7
9elmaz9 <132524901+9elmaz9@users.noreply.github.com>
2023-12-07 14:36:59 +0100

    Add files via upload

---
+++ Chapter15_FileIO/src/Les2/InputStreamExample.java
@@ -0,0 +1,38 @@
+package Les2;
+
+import java.io.*;
+
+public class InputStreamExample {
+
+    public static void main(String[] args) {
+        String sourseFile = "C:\\Users\\ELMAZ\\Desktop\\oneDrive\\Документы\\text1.txt";
+
+        String destinationFile =  "C:\\Users\\ELMAZ\\Desktop\\oneDrive\\Документы\\newtest.txt";
+
+        try (InputStream inputStream = new FileInputStream(sourseFile);
+             OutputStream outputStream = new FileOutputStream(destinationFile)) {
+            byte[] buffer = new byte[1024];
+            int byteRead;
+
+            while ((byteRead = inputStream.read(buffer)) != -1) {
+                outputStream.write(buffer, 0, byteRead);
+            }
+
+            System.out.println("File copied successfully");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+// "C:\Users\ELMAZ\Desktop\oneDrive\Документы"
+// "C:\\Users\\hlldm\\Documents\\newtext.txt";
