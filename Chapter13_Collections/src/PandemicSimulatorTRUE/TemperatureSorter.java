66e187617aa97facb680ec2660f7064f2cd0afe6
9elmaz9 <132524901+9elmaz9@users.noreply.github.com>
2023-12-12 09:43:09 +0100

    Add files via upload

---
+++ Chapter13_Collections/src/PandemicSimulatorTRUE/TemperatureSorter.java
@@ -0,0 +1,13 @@
+package PandemicSimulatorTRUE;
+
+import java.util.Comparator;
+
+
+
+    public class TemperatureSorter implements Comparator<Patient> {
+
+        @Override
+        public int compare(Patient o1, Patient o2) {
+            return o1.getTemperature() - o2.getTemperature();
+        }
+}
