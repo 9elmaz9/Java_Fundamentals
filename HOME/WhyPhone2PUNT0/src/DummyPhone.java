331aadf98254048a4b5c82b70253132a60573d65
9elmaz9 <132524901+9elmaz9@users.noreply.github.com>
2023-12-12 10:59:23 +0100

    Add files via upload

---
+++ HOME/WhyPhone2PUNT0/src/DummyPhone.java
@@ -0,0 +1,27 @@
+public class DummyPhone {
+
+
+    public class dummyPhone implements ISimCard, IRadio{
+        @Override
+        public void playChannel(double fm) {
+            System.out.println("Radio playing channel "+fm);
+        }
+
+        @Override
+        public void changeChannel(double fm) {
+            System.out.println("Radio channel changed to "+fm);
+        }
+
+        @Override
+        public void startCall(String number) {
+            System.out.println("Calling "+number);
+        }
+
+        public void showSpecs() {
+            startCall("0444 66 55 33");
+            endCall();
+            playChannel(22.77);
+            changeChannel(33.55);
+        }
+    }
+}
