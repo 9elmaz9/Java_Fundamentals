3cd2f074046a8feb160e3ddcd923a074beb58e9c
9elmaz9 <132524901+9elmaz9@users.noreply.github.com>
2023-12-12 09:54:38 +0100

    Delete Chapter13_Collections/src/ProductProjectCollections/ElectricProduct.java

--- Chapter13_Collections/src/ProductProjectCollections/ElectricProduct.java
+++
@@ -1,7 +0,0 @@
-package ProductProjectCollections;
-
-public class ElectricProduct extends Product{
-    public ElectricProduct(String name, int productID) {
-        super(name, productID);
-    }
-}
