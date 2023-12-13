bd1f853de84631500125270302c83acd5f00195d
9elmaz9 <132524901+9elmaz9@users.noreply.github.com>
2023-12-13 15:28:00 +0100

    Add files via upload

--- Chapter16_Multithreading/src/LabwerkMultiThreading/ThreadVoorbeeld.java
+++ Chapter16_Multithreading/src/LabwerkMultiThreading/ThreadVoorbeeld.java
@@ -1,9 +1,67 @@
 package LabwerkMultiThreading;
 
-public  class ThreadVoorbeeld extends Thread{
-    @Override
-    public void run(){
+//een nieuwJava klasse
+public class ThreadVoorbeeld {
 
+    private int count = 0;
+
+
+    public static void main(String[] args) {
+        ThreadVoorbeeld voorbeeld = new ThreadVoorbeeld();
+        InnerThread thread1 = new InnerThread(voorbeeld);
+        InnerThread thread2 = new InnerThread(voorbeeld);
+        InnerThread thread3 = new InnerThread(voorbeeld);
+
+
+        thread1.start();
+        try {
+            thread1.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        thread2.start();
+        try {
+            thread2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        thread3.start();
+
+        System.out.println("Hallo vanaf de hoofdthread");
 
     }
+
+    private static class InnerThread extends Thread {
+        //gebruik synchronized
+
+        private ThreadVoorbeeld voorbeeld;
+
+        public InnerThread(ThreadVoorbeeld voorbeeld) {
+            this.voorbeeld = voorbeeld;
+
+        }
+
+        @Override
+        public void run() {
+            voorbeeld.incrementCount();
+            System.out.println("Hallo vanaf thread" + this.getId() + " ,count is nu " + voorbeeld.getCount());
+
+              try {
+                  Thread.sleep(1000);
+              } catch (InterruptedException e) {
+                  e.printStackTrace();
+              }
+        }
+    }
+
+
+    public synchronized void incrementCount() {
+        count++;
+    }
+
+    public int getCount() {
+        return count;
+    }
 }
+
+
