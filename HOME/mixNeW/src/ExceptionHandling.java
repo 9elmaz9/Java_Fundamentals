b7ea35fc8a4439199ede307980c939d3abafbe1d
9elmaz9 <132524901+9elmaz9@users.noreply.github.com>
2023-12-16 22:32:50 +0100

    Add files via upload

---
+++ HOME/mixNeW/src/ExceptionHandling.java
@@ -0,0 +1,31 @@
+public class ExceptionHandling {
+        public static void main(String[] args) {
+
+            System.out.println("Program starts");           // Executed
+
+            int[] myArray = {3, 9, 45, 22, 16};
+
+            try {
+                int result = myArray[1] / 0;                   // Arithmetic Exception
+
+                System.out.println(myArray[1]);                // ArrayIndexOutOfBoundException, App crashed
+            } catch (ArrayIndexOutOfBoundsException | ArithmeticException exception) {
+                // Your code.. Optional
+                System.out.println(exception);
+            } finally {
+                // your code.. Optional
+                System.out.println("The finally block is always executed");
+            }
+
+            try {
+                String name = null; // No object
+                System.out.println(name.length());
+            } catch (Exception exception) {         // General way to handle the exception
+                System.out.println(exception);
+            }
+
+            System.out.println("Program ends");             // Executed .
+        }
+    }
+
+}
