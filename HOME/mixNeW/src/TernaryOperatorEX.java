c056c4311dde599d46acba3bfb68b69f835e1c67
9elmaz9 <132524901+9elmaz9@users.noreply.github.com>
2023-12-16 22:49:30 +0100

    Add files via upload

---
+++ HOME/mixNeW/src/TernaryOperatorEX.java
@@ -0,0 +1,12 @@
+public class TernaryOperatorEX {
+
+    public static void main(String[] args) {
+
+        int a = 2;
+        int b = 3;
+
+        int min = (a < b)? a : b;
+
+        System.out.println(min);
+    }
+}
