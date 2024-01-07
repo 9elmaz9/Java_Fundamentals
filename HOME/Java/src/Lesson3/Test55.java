c38d1a055f438ad2a67901add26834e1cf82708d
9elmaz9 <132524901+9elmaz9@users.noreply.github.com>
2024-01-07 21:31:19 +0100

    Add files via upload

---
+++ HOME/Java/src/Lesson3/Test55.java
@@ -0,0 +1,18 @@
+package Lesson3;
+
+public class Test55 {
+    public static void main(String[] args) {
+
+        //множественное присвоение
+
+        int a = 3;
+        int b = 50;
+        int c = 0;
+
+        a=b=c=18;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+
+}
